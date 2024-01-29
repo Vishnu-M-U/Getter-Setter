1485f070678c93a99ebdf9500b609855098be22a
Vishnu-M-U <119624479+Vishnu-M-U@users.noreply.github.com>
2024-01-29 14:05:18 +0530

    Add files via upload

---
+++ Getter and Setter/src/Input.java
@@ -0,0 +1,10 @@
+public class Input {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String x){
+        this.name=x;
+
+    }
+}
---
+++ Getter and Setter/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Input obj=new Input();
+        obj.setName("vishnu");
+        System.out.println("my name is "+obj.getName());
+    }
+}
